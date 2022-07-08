f5fa5ac354742c2ea3f4438320fc2fd2ce4d44de
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-08 08:22:40 +0000

    Added FizzBuzz to app.js

--- KataTwo/app.js
+++ KataTwo/app.js
@@ -1,4 +1,4 @@
-let n = 100;
+let n = 50;
 
 for (let i = 0; i < n; i++) {
     if (i % 15 == 0) {
@@ -11,3 +11,38 @@ for (let i = 0; i < n; i++) {
         console.log(i);
     }
 }
+
+/*__________________________FizzBuzz__________________________*/
+
+
+function splitOnDoubleLetter(word) {
+  let previousLetter = word[0];
+  let placeHolder = 0;
+  let result = [];
+  let splitWord = "";
+  let matchFound = false;
+
+  for (let i = 1; i < word.length; i++) {
+    // previousLetter = word[i];
+
+    if (previousLetter == word[i]) {
+      splitWord = word.substring(placeHolder, i);
+      result.push(splitWord)
+      placeHolder = i;
+      matchFound = true;
+    }
+
+    result.push(word.substring(placeHolder, word.length))
+    // previousLetter = word[i];
+
+    if (matchFound == true) {
+      result.push(word.substring(placeHolder, word.length));
+      word.substring();
+    }
+  }
+  return result;
+}
+
+console.log(splitOnDoubleLetter("happy"));
+console.log(splitOnDoubleLetter("easy"));
+
