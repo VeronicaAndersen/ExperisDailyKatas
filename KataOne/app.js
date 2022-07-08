cf724e08c605a0bf6700bdba29046a5ab5b1a86c
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-08 10:37:55 +0200

    Added second part of kata one

--- KataOne/app.js
+++ KataOne/app.js
@@ -10,14 +10,44 @@ points(7, 5);
 // function with parameters that calculate the points and the return it.
 function points(twoPointer, threePointer) {
   finalPoints = twoPointer * 2 + threePointer * 3;
-return finalPoints;
+  return finalPoints;
 }
 // console.log(finalPoints);
 document.getElementById("points").innerHTML = "Total points: " + finalPoints;
 
 
 
+/*__________________________FizzBuzz__________________________*/
 
 
+function splitOnDoubleLetter(word) {
+  let previousLetter = word[0];
+  let placeHolder = 0;
+  let result = [];
+  let splitWord = "";
+  let matchFound = false;
 
+  for (let i = 1; i < word.length; i++) {
+    // previousLetter = word[i];
+
+    if (previousLetter == word[i]) {
+      splitWord = word.substring(placeHolder, i);
+      result.push(splitWord)
+      placeHolder = i;
+      matchFound = true;
+    }
+
+    result.push(word.substring(placeHolder, word.length))
+    // previousLetter = word[i];
+
+    if (matchFound == true) {
+      result.push(word.substring(placeHolder, word.length));
+      word.substring();
+    }
+  }
+  return result;
+}
+
+console.log(splitOnDoubleLetter("happy"));
+console.log(splitOnDoubleLetter("easy"));
 
